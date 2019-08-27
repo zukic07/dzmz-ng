4471aef097c6def27ec44fa0f3f0dd42267df8f4
Ahmed Zukic <ahmed.zukic@softwareag.com>
2019-08-27 16:43:31 +0200

    fix main menu animation

--- src/app/animations/main-menu.ts
+++ src/app/animations/main-menu.ts
@@ -2,7 +2,34 @@ import { trigger, style, query, group, animate, animateChild, transition } from 
 
 export const mainMenuToggle = 
 trigger('routeAnimations', [
-    transition('Home <=> News', [
+    transition('News => Home', [
+        query(':enter, :leave', [
+            style({
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%'
+            })
+        ]),
+        query(':enter', [
+            style({ left: '100%' })
+        ]),
+        query(':leave', [
+            style({ left: '0' })
+        ]),
+        group([
+            query(':enter', [
+                animate('300ms', style({ left: '0%' })) 
+            ]),
+            query(':leave', [
+                animate('300ms', style({ left: '-100%' }))
+            ])
+        ]),
+        query(':enter', animateChild()),
+        query(':leave', animateChild())
+    ])
+    ,    
+    transition('Home => News', [
         query(':enter, :leave', [
             style({
                 position: 'absolute',
@@ -19,10 +46,10 @@ trigger('routeAnimations', [
         ]),
         group([
             query(':leave', [
-                animate('500ms', style({ left: '100%' }))
+                animate('300ms', style({ left: '100%' }))
             ]),
             query(':enter', [
-                animate('500ms', style({ left: '0%' })) 
+                animate('300ms', style({ left: '0%' })) 
             ])
         ]),
         query(':enter', animateChild()),
